1fcebdfc8beebaec928fcfac2f827ca47b317506
dogebi <rainxlh@gmail.com>
2024-12-11 13:51:23 +0800

    add host

--- app.py
+++ app.py
@@ -39,4 +39,4 @@ def ask_gpt():
         return jsonify({"success": False, "error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5500,debug=True)
