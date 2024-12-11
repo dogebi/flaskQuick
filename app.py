6d243cd8e82963ccca989fe257c7e539945d3309
dogebi <rainxlh@gmail.com>
2024-12-11 20:30:21 +0800

    update

--- app.py
+++ app.py
@@ -2,14 +2,24 @@ import os
 import openai
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from dotenv import load_dotenv
+
 
 # Flask 애플리케이션 초기화
 app = Flask(__name__)
 CORS(app)
 
 # Azure OpenAI API 키와 엔드포인트를 환경 변수에서 가져옵니다.
-openai.api_key = os.getenv('AZURE_OPENAI_KEY')  # Azure OpenAI API Key
-openai.api_base = os.getenv('AZURE_OPENAI_ENDPOINT')  # Azure OpenAI Endpoint
+openai.api_key = os.environ.get('AZURE_OPENAI_KEY')  # Azure OpenAI API Key
+# openai.api_base = os.environ.get('AZURE_OPENAI_ENDPOINT')  # Azure OpenAI Endpoint
+AZURE_OPENAI_SERVICE = os.environ.get("AZURE_OPENAI_SERVICE")
+AZURE_OPENAI_API_VERSION = os.environ.get("AZURE_OPENAI_API_VERSION")
+
+openai.api_base = f"https://{AZURE_OPENAI_SERVICE}.openai.azure.com"
+openai.api_version = AZURE_OPENAI_API_VERSION
+
+# Comment these two lines out if using keys, set your API key in the AZURE_OPENAI_API_KEY environment variable instead
+openai.api_type = "azure"
 
 # # 환경 변수에서 프록시 설정 가져오기
 # os.environ['HTTP_PROXY'] = 'http://80023:nomura49@172.19.248.1:92'
@@ -43,6 +53,7 @@ def ask_gpt():
     except Exception as e:
         app.logger.error(f"Error: {str(e)}")
         return jsonify({"success": False, "error": str(e)}), 500
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
